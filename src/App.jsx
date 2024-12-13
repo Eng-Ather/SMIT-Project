508e390b629542075882ea8a102f309a1e5a32f4
aamir <aamirchanna127@gmail.com>
2024-12-12 22:25:33 -0800

    create error  and teacher page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Notifications from "./pages/Notification";
 import Footer from "./components/Footer";
+import Teacher from "./pages/Teacher";
+import Error from "./pages/Error";
 
 function App() {
   return (
@@ -15,8 +17,10 @@ function App() {
         {/* <Route path=" pageName" element={< component name created inside page />} /> */}
 
           <Route path="/" element={<Home />} />
+          <Route path="/*" element={<Error />} />
           <Route path="/About" element={<About />} />
           <Route path="/Notification" element={<Notifications />} />
+          <Route path="/teacher" element={<Teacher />} />
         </Routes>
       </div>
       <Footer />
